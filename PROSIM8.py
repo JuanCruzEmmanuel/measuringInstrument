cd0698fa4ae72a91de10f8a4d624fd3008e364fd
JuanCruzEmmanuel <juancruznoya@mi.unc.edu.ar>
2025-04-28 17:33:03 -0300

    se sube bibliografia para control prosim8 y un inicio de conexion

--- PROSIM8.py
+++ PROSIM8.py
@@ -4,15 +4,24 @@ import numpy
 from INSTRUCONTRACT import instru_contract
 
 class PROSIM8(instru_contract):
-    def __init__(self,port,baudare=9600):
+    def __init__(self,port,baudare=115200):
         self.port = port
         self.baudrate = baudare
-    
+        self.con = None
     def connect(self):
-        pass
+        """
+        CONECTA PROSIM8 CON PUERTO SERIE\n
+        DATOS:\n
+        serial.STOPBITS_ONE = 1\n
+        serial.PARITY_NONE = 'None'\n
+        """
+        self.con = serial.Serial(port=self.port,baudrate=self.baudrate,stopbits=serial.STOPBITS_ONE, parity=serial.PARITY_NONE,bytesize=8,xonxoff=True)
 
     def disconnect(self):
-        pass
+        """
+        DESCONECTA PROSIM8 CON PUERTO SERIE\n
+        """
+        self.con.close()
 
     def readcommand(self):
         pass
