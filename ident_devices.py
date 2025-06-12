3b619377fd74eb6280c6a867c74fcf7f30a7e3f6
jfont <jfont@feaselectronica.com.ar>
2025-06-12 13:07:03 -0300

    Ident de PSU y Multimeter.

--- ident_devices.py
+++ ident_devices.py
@@ -1,25 +1,32 @@
 from serial.tools import list_ports
-import serial, os, json
+import serial, os, json, struct
+from DCPOWERSUPPLY import PSU
 
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-print(BASE_DIR)
-JSON_FILE = os.path.join(BASE_DIR, "_TEMPS_\devices.json")
-print(JSON_FILE)
+JSON_FILE_PATH = os.path.join(BASE_DIR, "_TEMPS_\devices.json")
 
-def ident_devices():
+
+def ident_devices(debug=False,JSON_FILE=JSON_FILE_PATH):
     used_ports = list_ports.comports()
+    rta_1 = ""
+    rta_2 = ""
+    rta_3 = ""
+    
+    if debug:
+        print("\nPuertos encontrados:")
+        for p in used_ports:
+            print(p.description)
 
-    if os.path.exists(JSON_FILE):
-        return
+    found_devices = []
 
     for port in used_ports:
         baudrates = (9600, 38400, 115200)
-        found_devices = []
+
 
         for baudrate in baudrates:
+            respuesta = ""
             try:
-
                 connection = serial.Serial(
                     port=port.device,
                     baudrate=baudrate,
@@ -28,16 +35,52 @@ def ident_devices():
                     bytesize=8,
                     timeout=0.1,
                     write_timeout=0.1)
-                
+
+                # Intento identificar ESA620
                 connection.write("REMOTE\r".encode())
                 connection.readline().decode()
                 connection.write("IDENT\r".encode())
-                respuesta = connection.readline().decode()
+                rta_1 = connection.readline().decode()
+                if "ESA 620" in rta_1:
+                    respuesta = rta_1
+
+                # Intento identificar Fluke8845A y Fluke45
+                connection.write("*IDN?\r\n".encode())
+                rta_2 = connection.readline().decode()
+                if "8845A" in rta_2:
+                    respuesta = rta_2
+                if "45" in rta_2 and "88" not in rta_2:
+                    respuesta = rta_2
+
+                # Intento identificar Fuente Array/Protek
+                if connection and connection.is_open:
+                    connection.close()
+                    try:
+                        psu = PSU(0,port.device,baudrate)
+                        rta_3 = psu.get_info()
+                        print(rta_3)
+                    except:
+                        pass
+
+
+
+                if debug:
+                    print(f"Respuesta de {port.device} a {baudrate} baudios: {respuesta}")
 
                 if not respuesta == "":
+                    if "ESA 620" in respuesta:
+                        device = "ESA620"
+                    elif "8845A" in respuesta:
+                        device = "8845A"
+                    elif "45" in respuesta and "88" not in respuesta:
+                        device = "45"
+                    else:
+                        device = "Desconocido"
+
                     device_info = {
                         "port": port.device,
-                        "device": respuesta
+                        "baudrate": baudrate,
+                        "device": device
                         }
                     found_devices.append(device_info)
 
@@ -48,16 +91,15 @@ def ident_devices():
                 print(f"Error de conexión: {e}")
                 pass
 
-        if found_devices:
-            try:
-                with open(JSON_FILE, "w") as file:
-                    json.dump(found_devices, file, indent=4)
-            except IOError as e:
-                print(f"Error al escribir el archivo JSON: {e}")
+    if found_devices:
+        try:
+            with open(JSON_FILE, "w") as file:
+                json.dump(found_devices, file, indent=4)
+        except IOError as e:
+            print(f"Error al escribir el archivo JSON: {e}")
 
 
 
 
 
-
-ident_devices()
+ident_devices(debug=True, JSON_FILE=JSON_FILE_PATH)
